66f8c74dcfd0c03512f654827aaf2a3614978cac
Rohit Khatri <developer.rohitkhatri@gmail.com>
2024-03-29 10:09:07 +0530

    Displayed critic score

--- src/services/game-service.ts
+++ src/services/game-service.ts
@@ -12,6 +12,7 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: GamePlatform }[];
+  metacritic: number;
 }
 
 class GameService extends HttpService {
