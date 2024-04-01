ab8a0db8c2413d1915bf3fd17a2b5005971077c4
Rohit Khatri <developer.rohitkhatri@gmail.com>
2024-04-01 23:19:50 +0530

    Search input added

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -9,7 +9,7 @@ export default function ColorModeSwitch() {
         isChecked={colorMode === 'dark'}
         onChange={toggleColorMode}
       ></Switch>
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 }
